06ca770c3c14850e45293bb3448b5725e57bd86a
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-05-12 23:58:02 -0300

    feat: filtro nos documentos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,12 +2,28 @@ import db from "../mongoDB.js";
 import { ObjectId } from "mongodb";
 
 async function getProductsTelaInicial(req, res) {
-    try {
-        const produtosCadastrados = await db.collection('products').find().toArray();
-        return res.status(200).send(produtosCadastrados);
-    } catch (error) {
-        console.log('Catch conexão banco de dados', error);
-        res.sendStatus(500);
+    const { filtro } = req.query;
+    
+    if(filtro) {
+        const filtroTitle = filtro.charAt(0).toUpperCase() + filtro.slice(1);
+        console.log(filtro, filtroTitle);
+        
+        const produtos = await db.collection('products').find().toArray();
+        const produtosFiltrados = produtos.filter(produto => {
+            return produto.name.includes(filtroTitle) || produto.description.includes(filtroTitle) || 
+            produto.category.includes(filtroTitle) || produto.price.includes(filtroTitle) ||
+            produto.name.includes(filtro) || produto.description.includes(filtro) || 
+            produto.category.includes(filtro) || produto.price.includes(filtro);
+        });
+        return res.status(200).send(produtosFiltrados);
+    }else{
+        try {
+            const produtosCadastrados = await db.collection('products').find().toArray();
+            return res.status(200).send(produtosCadastrados);
+        } catch (error) {
+            console.log('Catch conexão banco de dados', error);
+            res.sendStatus(500);
+        }
     }
 }
 

8d8dcc2ce522c2ba2ec2dc00e9a35f3845a63780
Victor Matos <antoniovitomatos@gmail.com>
2022-05-14 11:29:04 -0300

    atualização carrinho

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -36,7 +36,7 @@ export async function removeCart(req, res) {}
 
 export async function addCart(req, res) {
     const {id}=req.params;
-    const {product}=req.body;
+    const product=req.body;
     try{
         const cart = await db.collection('carts').findOne({id});
         if(!cart){
@@ -46,8 +46,8 @@ export async function addCart(req, res) {
         // if(data){
         //     return res.status(404).send('Produto já adicionado');
         // }
-        const productCart = await db.collection('productsCart').insertOne({id, product});
-        res.sendStatus(201);
+        const productCart = await db.collection('productsCart').insertOne({id, ...product});
+        res.send(productCart).status(201);
     }catch (error) {
         console.log(chalk.red('Catch conexão banco de dados'), error);
     }
@@ -55,6 +55,51 @@ export async function addCart(req, res) {
 
 
 export async function clearCart(req, res) {}
+
+export async function removeCartProduct(req, res) {
+    const {id}=req.params;
+    const {idProduct}=req.body; 
+    console.log(id, idProduct);
+    try{
+        const cart = await db.collection('carts').findOne({id});
+        if(!cart){
+            return res.status(404).send('Carrinho não encontrado');
+        }
+        const data = await db.collection('productsCart').findOne({_id: new ObjectId(idProduct)});
+        if(!data){
+            return res.status(404).send('Produto não encontrado');
+        }
+        const productCart = await db.collection('productsCart').deleteOne({_id: new ObjectId(idProduct)});
+        const products = await db.collection('productsCart').find({id}).toArray();
+        res.send(products).status(201);
+    }catch (error) {
+        console.log(chalk.red('Catch conexão banco de dados'), error);
+    }
+}
+
+export async function updateCart(req, res) {
+    const {id}=req.params;
+    const {idProduct, amountItem}=req.body;
+    console.log(req.body);
+    try{
+        const cart = await db.collection('carts').findOne({id});
+        if(!cart){
+            return res.status(404).send('Carrinho não encontrado');
+        }
+        const productsCart = await db.collection('productsCart').findOne({_id: new ObjectId(idProduct)});
+        if(!productsCart){
+            return res.status(404).send('Produto não encontrado');
+        }
+        await db.collection('productsCart').updateOne({_id: new ObjectId(idProduct)}, {$set:{quantidade:amountItem} });
+        console.log(chalk.green.bold("Quantidade alterada", idProduct, amountItem));
+        
+
+        const products = await db.collection('productsCart').find({id}).toArray();
+        res.send(products).status(201);
+    }catch (error) {
+        console.log(chalk.red('Catch conexão banco de dados'), error);
+    }
+}
 
 export async function getCartTotal(req, res) {
     const {id}=req.params;
--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import {getCart, addCart, createCart, getCartAmount} from './../controllers/cartController.js';
+import {getCart, addCart, createCart, getCartAmount, updateCart, removeCartProduct} from './../controllers/cartController.js';
 
 const cartRouter = Router();
 
@@ -8,5 +8,6 @@ cartRouter.get('/cart/amount/:id', getCartAmount);
 cartRouter.get('/cart/getCart/:id', getCart);
 cartRouter.post('/cart/create', createCart);
 cartRouter.post('/cart/add/:id', addCart);
-
+cartRouter.post('/cart/newamount/:id', updateCart);
+cartRouter.post('/cart/remove/:id', removeCartProduct);
 export default cartRouter;
